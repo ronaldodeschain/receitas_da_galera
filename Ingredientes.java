33cf4105b7ded25bd5561d8901beb6c0b32e600d
Donalson-pdjour <holojk9@gmail.com>
2025-11-14 19:46:15 -0300

    atualização equipe1

--- Ingredientes.java
+++ Ingredientes.java
@@ -1,5 +1,42 @@
 public class Ingredientes {
+    private String nomeIngrediente;
+    private int quantidadeingredientes;
+    private Boolean perecivel;
 
-    private String nome;
-    
+
+public Ingredientes(String nomeIngrediente, int quantidadeingredientes, boolean perecivel){
+    this.nomeIngrediente = nomeIngrediente;
+    this.quantidadeingredientes = quantidadeingredientes;
+    this.perecivel = perecivel;
+}
+
+    public String getNomeIngrediente() {
+        return nomeIngrediente;
+    }
+
+    public void setNomeIngrediente(String nomeIngrediente) {
+        this.nomeIngrediente = nomeIngrediente;
+    }
+
+    public int getQuantidadeingredientes() {
+        return quantidadeingredientes;
+    }
+
+    public void setQuantidadeingredientes(int quantidadeingredientes) {
+        this.quantidadeingredientes = quantidadeingredientes;
+    }
+
+    public Boolean getPerecivel() {
+        return perecivel;
+    }
+
+    public void setPerecivel(Boolean perecivel) {
+        this.perecivel = perecivel;
+    } 
+
+    @Override
+    public String toString() {
+        return "Ingredientes [nomeIngrediente=" + nomeIngrediente + ", quantidadeingredientes=" + quantidadeingredientes
+                + ", perecivel=" + perecivel + "]";
+    }
 }
--- Receita.java
+++ Receita.java
@@ -1,5 +1,63 @@
+import java.util.*;
+
 public class Receita {
-    
-    private int tempoReceita;
+    private String nomeReceita;
+    private int tempPreparo;
+    private String tipoReceita;
+    private String modoDepreparo;
+    private List<Ingredientes> listaDeIngredientes = new ArrayList<>();
+
+    public void AdicionarIngrediente(Ingredientes ingrediente) {
+        listaDeIngredientes.add(ingrediente);
+    }
+
+    public Receita(String nomeReceita, int tempPreparo, String tipoReceita, String modoDepreparo) {
+        this.nomeReceita = nomeReceita;
+        this.tempPreparo = tempPreparo;
+        this.tipoReceita = tipoReceita;
+        this.modoDepreparo = modoDepreparo;
+    }
+
+    public List<Ingredientes> listaIngredientes() {
+        return listaDeIngredientes;
+    }
+
+    public String getNomeReceita() {
+        return nomeReceita;
+    }
+
+    public void setNomeReceita(String nomeReceita) {
+        this.nomeReceita = nomeReceita;
+    }
+
+    public int getTempPreparo() {
+        return tempPreparo;
+    }
+
+    public void setTempPreparo(int tempPreparo) {
+        this.tempPreparo = tempPreparo;
+    }
+
+    public String getTipoReceita() {
+        return tipoReceita;
+    }
+
+    public void setTipoReceita(String tipoReceita) {
+        this.tipoReceita = tipoReceita;
+    }
+
+    public String getModoDepreparo() {
+        return modoDepreparo;
+    }
+
+    public void setModoDepreparo(String modoDepreparo) {
+        this.modoDepreparo = modoDepreparo;
+    }
+
+    @Override
+    public String toString() {
+        return "Receitas [nomeReceita=" + nomeReceita + ", tempPreparo=" + tempPreparo + ", tipoReceita=" + tipoReceita
+                + ", modoDepreparo=" + modoDepreparo + ", listaDeIngredientes=" + listaDeIngredientes + "]";
+    }
 
 }
